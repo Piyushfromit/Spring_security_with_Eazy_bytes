e0db6fd378e60b4a0cfd39a6e1893e0d67546427
piyushfromit <piyushfrommasai@gmail.com>
2024-08-27 09:04:53 +0530

    session 6 complated

---
+++ springsecsection7/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.requiresChannel(requestChannelConfiguration -> requestChannelConfiguration.anyRequest().requiresInsecure())
+                .csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/myAccount", "/myBalance", "/myLoans", "/myCards", "/welcome").authenticated()
+                .requestMatchers("/notices", "/contact", "/error", "/logout", "/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // not to use simple password
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+
+}
