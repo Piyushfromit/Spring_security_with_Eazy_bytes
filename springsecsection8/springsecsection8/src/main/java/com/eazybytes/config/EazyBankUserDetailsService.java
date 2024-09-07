642a548f294ca5522f8efbed2c2f39a5927a4f9f
piyushfromit <piyushfrommasai@gmail.com>
2024-09-07 08:09:55 +0530

    jgh

---
+++ springsecsection8/springsecsection8/src/main/java/com/eazybytes/config/EazyBankUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.eazybytes.config;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository  customerRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Customer  customer = customerRepository.findByEmail(username).orElseThrow((() -> new UsernameNotFoundException("User not found")));
+        List<GrantedAuthority>  grantedAuthorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+
+        return new User(customer.getEmail(), customer.getPwd(), grantedAuthorities);
+
+    }
+
+
+}
---
+++ springsecsection8/springsecsection8/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -0,0 +1,51 @@
+package com.eazybytes.config;
+
+import com.eazybytes.exceptionhandling.CustomAccessDeniedHandler;
+import com.eazybytes.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("prod")
+public class ProjectSecurityProdConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(sessionManagementConfiguration ->  sessionManagementConfiguration.invalidSessionUrl("/invalidSession").maximumSessions(10).maxSessionsPreventsLogin(true))
+                .requiresChannel(requestChannelConfiguration -> requestChannelConfiguration.anyRequest().requiresSecure())
+                .csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/myAccount", "/myBalance", "/myLoans", "/myCards", "/welcome").authenticated()
+                .requestMatchers("/notices", "/contact", "/error", "/logout", "/register", "/invalidSession").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(httpBasicConfig -> httpBasicConfig.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        //http.exceptionHandling(exceptionHandlingConfig ->exceptionHandlingConfig.authenticationEntryPoint( new CustomBasicAuthenticationEntryPoint())); // It is an Global Config
+        http.exceptionHandling(exceptionHandlingConfig ->exceptionHandlingConfig.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        //http.exceptionHandling(exceptionHandlingConfig ->exceptionHandlingConfig.accessDeniedHandler(new CustomAccessDeniedHandler()).accessDeniedPage("/errorJspPage"));// for MVC or jsp included project
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // not to use simple password
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+
+}
