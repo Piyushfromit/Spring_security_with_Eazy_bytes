6bf4893610d7308ddfb3922133f2bc690fc6b61b
piyushfromit <piyushfrommasai@gmail.com>
2024-10-07 03:49:17 +0530

    jhacsdghsda

--- springsecsection12/springsecsection12/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecsection12/springsecsection12/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,11 +2,12 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-//@EnableWebSecurity(debug = true)
 @EnableWebSecurity
+@EnableMethodSecurity( prePostEnabled = true, jsr250Enabled = true, securedEnabled = true)
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
