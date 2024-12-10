08775b35fd2c0e5701c08225f26d113ccdda82f6
piyushfromit <piyushfrommasai@gmail.com>
2024-12-11 00:19:39 +0530

    jhg

---
+++ springsecsection11/springsecsection11/src/main/java/com/eazybytes/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.eazybytes.repository;
+
+import com.eazybytes.model.Authority;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorityRepository extends CrudRepository<Authority,Long> {
+
+
+
+}
